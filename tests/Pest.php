6d90316177eeece68336d4671160551f184b941a
Ashley Hindle <hey@ashleyhindle.com>
2025-06-18 13:25:01 +0100

    fix: conditional editor choices in tests
    https://github.com/laravel/framework/pull/52408

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,9 +2,15 @@
 
 use Croft\Tests\TestCase;
 
+use function Orchestra\Testbench\laravel_version_compare;
+
 uses(TestCase::class)->in(__DIR__);
 
 function getEditorChoices(): array
 {
-    return ['cursor' => 'Cursor', 'windsurf' => 'Windsurf', 'phpstorm' => 'PhpStorm (coming soon)', 'cursor', 'phpstorm', 'windsurf'];
+    $choices = ['cursor' => 'Cursor', 'windsurf' => 'Windsurf', 'phpstorm' => 'PhpStorm (coming soon)'];
+    if (laravel_version_compare('11.20', '<=')) {
+        $choices = array_merge($choices, ['cursor', 'phpstorm', 'windsurf']);
+    }
+    return $choices;
 }
