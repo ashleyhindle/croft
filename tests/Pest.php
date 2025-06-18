47f0767598b5b27f9252fc2755636156402a87c0
Ashley Hindle <ashleyhindle@users.noreply.github.com>
2025-06-18 13:54:26 +0100

    feat: add Laravel 10 support (#15)
    * feat: add initial CI changes for laravel 10 support
    
    * chore: update testbench version to 8 for laravel 10 tests
    
    * chore: exclude Laravel 12 on 8.1 tests
    
    * chore: exclude laravel 11 on php8.1 tests
    
    * fix: conditional editor choices in tests
    
    https://github.com/laravel/framework/pull/52408
    
    * Fix styling
    
    * chore: simplify test matrix with repetition
    
    * chore: conditional test based on laravel version
    
    * chore: fix CI with direct choice question call so no conditional required
    
    * chore: only call arch tests if available
    
    * chore: prefer stable only for 8.1 and 10
    
    ---------
    
    Co-authored-by: ashleyhindle <454975+ashleyhindle@users.noreply.github.com>

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,10 +1,13 @@
 <?php
 
 use Croft\Tests\TestCase;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 uses(TestCase::class)->in(__DIR__);
 
 function getEditorChoices(): array
 {
-    return ['cursor' => 'Cursor', 'windsurf' => 'Windsurf', 'phpstorm' => 'PhpStorm (coming soon)'];
+    $choices = new ChoiceQuestion('Which editor are you using?', ['cursor' => 'Cursor', 'windsurf' => 'Windsurf', 'phpstorm' => 'PhpStorm (coming soon)']);
+
+    return $choices->getAutocompleterValues();
 }
